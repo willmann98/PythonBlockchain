4f8549a4ccde32144185c57b942b02653e20eb6b
Willmann <williamfaaberg@gmail.com>
2021-03-17 19:04:52 +0100

    push

---
+++ Pblock.py
@@ -0,0 +1,61 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.pending_transactions = []
+
+        self.new_block(previous_hash="The Times 17.03.2021 and i am creating WIFcoin.", proof=100)
+
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.pending_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.pending_transactions = []
+        self.chain.append(block)
+
+        return block
+
+
+    @property
+    def last_block(self):
+ 
+        return self.chain[-1]
+
+
+    def new_transaction(self, sender, recipient, amount):
+        transaction = {
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        }
+        self.pending_transactions.append(transaction)
+        return self.last_block['index'] + 1
+
+
+    def hash(self, block):
+        string_object = json.dumps(block, sort_keys=True)
+        block_string = string_object.encode()
+
+        raw_hash = hashlib.sha256(block_string)
+        hex_hash = raw_hash.hexdigest()
+
+        return hex_hash
+
+
+blockchain = Blockchain()
+t1 = blockchain.new_transaction("William", "SIndre", '500 WIF')
+t2 = blockchain.new_transaction("Sindre", "William", '1 WIF')
+t3 = blockchain.new_transaction("William", "Sindre", '5 WIF')
+blockchain.new_block(12345)
+
+
+
+print("Genesis block: ", blockchain.chain)
